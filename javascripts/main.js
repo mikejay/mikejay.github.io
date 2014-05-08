4981b47fbba6d70cf49d7e09a3582f69ca1c128f
mikejay <642108441@qq.com>
2014-05-08 15:10:07 +0800

    修改个人博客

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,4 @@
 (function($){
-
 	var ins = {
 		token : '29561604.0d9e12b.f6b44a37388246918f27c9fa26b9a11c' ,
 		userId : '29561604' ,
@@ -8,6 +7,10 @@
 			var htmlStr = [] ; 
 				//设置nexturl
 				ins.nextUrl = d.pagination.next_url ;
+				if(typeof d.pagination.next_url == 'undefined'){
+					$('.life .load_more').hide();
+				}
+
 				if(d.data){
 					var data = d.data ;
 					for(var i = 0 ;i < data.length ; i++){
@@ -65,13 +68,58 @@
 			$.ajax( options );
 			$('.life .load_more').text('加载更多');
 		}
-	};
-	//ins.getUser();
-	ins.getRecntData();
+	};	
 
-	$('.life .load_more').click(function(){
-		$(this).text('加载中...');
-		ins.getNext();
-	})
+	var imgOperation = {
+		showBigImg : function( ){
+			var scrollT = $(document).scrollTop() ,
+				left = this.offset().left , 
+				top = this.offset().top - scrollT ,
+				tmpl = ($(window).width() - 640)/2 , 
+				tmpt = ($(window).height() - 640)/2 ,
+				l = tmpl > 0 ? tmpl : 0 , 
+				t = tmpt > 0 ? tmpt : 0 ,
+				newImg = this.parent().clone() ,
+				cover = $('.body_cover') ;
+
+				newImg.addClass('big_pic').css({ 'left' : left , 'top' : top });
+				$('<span class="close"><img src="'+window.location+'/images/close.png"></span>').appendTo(newImg);
+				newImg.appendTo('body');
+				cover.show();
+
+				newImg.stop().animate({
+					top : t , 
+					left : l , 
+					width : 650 , 
+					height : 650 
+				} , 500) ;
+		} , 
+		hideBigImg : function(cover){
+			this.remove();
+			cover.hide();
+		}
+	}
+
+	$(function(){
+		//ins.getUser();
+		ins.getRecntData();
+
+		$('.life .load_more').click(function(){
+			$(this).text('加载中...');
+			ins.getNext();
+		});
+
+		$('.image_list li img').live('click' , function(){
+			imgOperation.showBigImg.call( $(this) );
+		});
+
+		$('.body_cover , .big_pic .close').live('click' , function(){
+
+			imgOperation.hideBigImg.call( $('.big_pic') , $('.body_cover'));
+		});
+
+	});
+
+	
 
 })(jQuery);

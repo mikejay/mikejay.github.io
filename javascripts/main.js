977769f0afc7c3225123e633098646efd695147d
mikejay <apple@appletekiMacBook-Air.local>
2014-05-08 13:21:59 +0800

    修改个人博客

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,15 +2,32 @@
 
 	var ins = {
 		token : '29561604.0d9e12b.f6b44a37388246918f27c9fa26b9a11c' ,
-		//url : 'https://api.instagram.com/v1/users/3/media/recent/?access_token=' ,
+		userId : '29561604' ,
+		nextUrl : '' , 
+		callback : function(d){
+			var htmlStr = [] ; 
+				//设置nexturl
+				ins.nextUrl = d.pagination.next_url ;
+				if(d.data){
+					var data = d.data ;
+					for(var i = 0 ;i < data.length ; i++){
+						var url = data[i].images.standard_resolution.url ,
+							html = '<li><a href="javascript:;"><img src="'+ url +'" class="border_outside"/></a></li>' ;
+
+						htmlStr.push(html);
+					}
+					$(htmlStr.join(' ')).appendTo($('.image_list ul'));
+					//$('.image_list ul').html(htmlStr.join(' '));
+				}
+		} , 
 		getRecntData : function(){
 			var options = {
-				url : 'https://api.instagram.com/v1/users/3/media/recent/?access_token=' + this.token ,
+				url : 'https://api.instagram.com/v1/users/'+ins.userId+'/media/recent/?access_token=' + this.token ,
 				type : 'get' , 
 				dataType : 'jsonp' , 
 				data : {}  ,
 				success : function(d){
-					console.log(d);
+					ins.callback(d);
 				} , 
 				error : function(){
 					alert('data loading fail');
@@ -34,10 +51,27 @@
 				}
 			}
 			$.ajax( options );
-		} 
+		} ,
+		getNext : function(){
+			var options = {
+				url : ins.nextUrl , 
+				type : 'get' ,
+				dataType : 'jsonp' , 
+				data : {} , 
+				success : function(d){
+					ins.callback(d);
+				} , 
+			}
+			$.ajax( options );
+			$('.life .load_more').text('加载更多');
+		}
 	};
+	//ins.getUser();
+	ins.getRecntData();
 
-	ins.getUser();
-
+	$('.life .load_more').click(function(){
+		$(this).text('加载中...');
+		ins.getNext();
+	})
 
 })(jQuery);

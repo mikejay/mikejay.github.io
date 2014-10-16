8c86e489f301a8b8de3c0121653513362a9aac9c
刘雄杰 <mikejayLiu@gmail.com>
2014-10-16 15:54:50 +0800

    new share img

--- daily/script/daily.js
+++ daily/script/daily.js
@@ -217,7 +217,7 @@ head.js('script/lib/zepto.min.js' , 'script/lib/idangerous.swiper-2.1.min.js' , 
 
 	var wxData = {
 	  'appId': '', // 服务号可以填写appId，没有则留空
-	  'imgUrl': cover , // 分享显示的图标
+	  'imgUrl': shareImg , // 分享显示的图标
 	  'link': document.location.href, // 分享链接
 	  'title': '南方周末 － 今日专稿', // 分享标题
 	  'desc': title // 分享内容
